d192311eef4f112072436399107db42e041192b6
bibliophagist <viktorkozlovkonst@gmail.com>
2020-10-05 21:27:25 +0300

    video nine email sender

--- third.tasks/src/main/java/org/example/third/tasks/controller/RegistrationController.java
+++ third.tasks/src/main/java/org/example/third/tasks/controller/RegistrationController.java
@@ -1,21 +1,19 @@
 package org.example.third.tasks.controller;
 
-import org.example.third.tasks.model.Role;
 import org.example.third.tasks.model.User;
-import org.example.third.tasks.repository.UserRepository;
+import org.example.third.tasks.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -23,15 +21,23 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(Map<String, Object> model, User user) {
-        User userFromDb = userRepository.findByUsername(user.getUsername());
-        if (userFromDb != null) {
-            model.put("message", "User already exists!");
+    public String addUser(Model model, User user) {
+        if (!userService.addUser(user)) {
+            model.addAttribute("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoleSet(Collections.singleton(Role.USER));
-        userRepository.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActive = userService.activateUser(code);
+
+        if (isActive)
+            model.addAttribute("message", "User was successfully activated");
+        else
+            model.addAttribute("message", "Activation code is not found");
+
+        return "login";
     }
 }
--- third.tasks/src/main/java/org/example/third/tasks/service/UserService.java
+++ third.tasks/src/main/java/org/example/third/tasks/service/UserService.java
@@ -1,18 +1,26 @@
 package org.example.third.tasks.service;
 
+import org.example.third.tasks.model.Role;
+import org.example.third.tasks.model.User;
 import org.example.third.tasks.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final MailSender mailSender;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, MailSender mailSender) {
         this.userRepository = userRepository;
+        this.mailSender = mailSender;
     }
 
     public UserRepository getUserRepository() {
@@ -22,5 +30,42 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoleSet(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+
+        userRepository.save(user);
+
+        if (!StringUtils.isEmpty(user.getEmail())) {
+            String message = String.format("Hello, %s! \n" +
+                            "Welcome to Third Tasks. Please visit next link http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+
+        if (user == null)
+            return false;
+
+        user.setActivationCode(null);
+
+        userRepository.save(user);
+        return true;
     }
 }
