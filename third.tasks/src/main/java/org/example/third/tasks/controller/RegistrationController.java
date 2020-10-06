6253d3f3cd1a22fe7c4eda857c335c3c3e8ab90d
bibliophagist <viktorkozlovkonst@gmail.com>
2020-10-06 17:10:48 +0300

    video twelve captcha and remember me

--- third.tasks/src/main/java/org/example/third/tasks/controller/RegistrationController.java
+++ third.tasks/src/main/java/org/example/third/tasks/controller/RegistrationController.java
@@ -1,24 +1,40 @@
 package org.example.third.tasks.controller;
 
 import org.example.third.tasks.model.User;
+import org.example.third.tasks.model.dto.CaptchaResponseDto;
 import org.example.third.tasks.service.UserService;
 import org.example.third.tasks.utils.ControllerUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    private static final String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
 
-    @Autowired
-    private UserService userService;
+    @Value("${recaptcha.secret}")
+    private String recaptchaSecret;
+
+    private final UserService userService;
+    private final RestTemplate restTemplate;
+
+    public RegistrationController(UserService userService, RestTemplate restTemplate) {
+        this.userService = userService;
+        this.restTemplate = restTemplate;
+    }
 
     @GetMapping("/registration")
     public String registration() {
@@ -26,12 +42,27 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+    public String addUser(@RequestParam("password2") String passwordConfirmation,
+                          @RequestParam("g-recaptcha-response") String captchaResponse,
+                          @Valid User user,
+                          BindingResult bindingResult, Model model) {
+        String url = String.format(CAPTCHA_URL, recaptchaSecret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url,
+                Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha");
+        }
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirmation);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirmation)) {
             model.addAttribute("passwordError", "Passwords are mismatched");
         }
 
-        if (bindingResult.hasErrors()) {
+        if (isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
             model.mergeAttributes(errors);
 
@@ -48,11 +79,13 @@ public class RegistrationController {
     public String activate(Model model, @PathVariable String code) {
         boolean isActive = userService.activateUser(code);
 
-        if (isActive)
+        if (isActive) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User was successfully activated");
-        else
+        } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found");
-
+        }
         return "login";
     }
 }
