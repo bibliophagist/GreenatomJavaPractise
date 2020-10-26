268f274ade42e4095d49d27a6c6321324880d154
bibliophagist <viktorkozlovkonst@gmail.com>
2020-10-26 14:29:28 +0300

    video 21 jar compilation

--- third.tasks.part.two/webpack.common.js
+++ third.tasks.part.two/webpack.common.js
@@ -4,7 +4,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
-    entry: path.join(__dirname, 'src', 'main', 'resources', 'static', 'js', 'main.js'),
+    entry: path.join(__dirname, 'src', 'main', 'resources', 'js', 'main.js'),
     devServer: {
         contentBase: './dist',
         compress: true,
@@ -50,7 +50,7 @@ module.exports = {
     ],
     resolve: {
         modules: [
-            path.join(__dirname, 'src', 'main', 'resources', 'static', 'js'),
+            path.join(__dirname, 'src', 'main', 'resources', 'js'),
             path.join(__dirname, 'node_modules'),
         ],
     }
